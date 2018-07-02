23c8d09b56d5dd7e49716294806000f8418299cb
Tanner Harman <harmtan2@gmail.com>
2018-07-02 13:53:27 -0600

    Changed plan_name to name

--- db/migrate/20180629203601_create_carrier_plans.rb
+++ db/migrate/20180629203601_create_carrier_plans.rb
@@ -1,7 +1,7 @@
 class CreateCarrierPlans < ActiveRecord::Migration[5.2]
   def change
     create_table :carrier_plans do |t|
-      t.string :plan_name
+      t.string :name
       t.float :data_amount
       t.decimal :data_price
 
