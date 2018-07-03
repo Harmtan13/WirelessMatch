d3116523c47cc942b357c8108e6ae21c8aed6afc
Tanner Harman <harmtan2@gmail.com>
2018-07-02 20:16:36 -0600

    Implimented view Functionality

--- app/controllers/carriers_controller.rb
+++ app/controllers/carriers_controller.rb
@@ -1,6 +1,58 @@
 class CarriersController < ApplicationController
+  before_action :set_carrier, only: [:edit, :update, :show, :destroy]
 
   def index
     @carriers = Carrier.all
+  end
+
+  def new
+    @carrier = Carrier.new
+  end
+
+  def create
+    @carrier = Carrier.new(carrier_params)
+
+    respond_to do |format|
+      if @carrier.save
+        format.html { redirect_to carriers_path, notice: 'New Carrier has been created!'}
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @carrier.update(carrier_params)
+        format.html {redirect_to carriers_path, notice: 'Carrier has been updated!'}
+      else
+        format.html {render :edit}
+      end
+    end
+  end
+
+  def show
+    @plans = @carrier.carrier_plans
+  end
+
+  def destroy
+    @carrier.destroy
+    respond_to do |format|
+      format.html { redirect_to carriers_path, notice: 'Carrier has been deleted'}
+    end
+  end
+
+
+  def set_carrier
+    @carrier = Carrier.find(params[:id])
+  end
+
+  def carrier_params
+    params.require(:carrier).permit(
+      :name
+      )
   end
 end
