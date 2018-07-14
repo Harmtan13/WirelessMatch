87e148e3742d3e763eca1054a20f4ce2cd7ffff5
Tanner Harman <harmtan2@gmail.com>
2018-07-14 14:58:39 -0600

    Added Results Path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+
   resources :user_plans
+  get 'results', to: 'user_plans#results', as: 'results_path'
+
   resources :carriers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
