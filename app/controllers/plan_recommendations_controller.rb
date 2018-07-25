2ec7c111f8dc1cf70eeb4c2bc49ec9c1cdc64348
Tanner Harman <harmtan2@gmail.com>
2018-07-25 14:19:47 -0600

    Added a plan reccomendation page

--- app/controllers/plan_recommendations_controller.rb
+++ app/controllers/plan_recommendations_controller.rb
@@ -1,2 +1,9 @@
 class PlanRecommendationsController < ApplicationController
+
+  def index
+    @carriers = Carrier.all
+    @quiz = UserPlan.last
+    @plan = PlanRecommendation.new(@carriers, @quiz)
+  end
+
 end
