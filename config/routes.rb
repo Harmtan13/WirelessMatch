f06c7692aa6a7acd9b166e1f6741ac27363ba8ed
Tanner Harman <harmtan2@gmail.com>
2018-09-06 21:10:50 -0600

    Removed the Carriers & Plans edit page from the routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :user_plans
   get 'results', to: 'user_plans#results', as: 'results_path'
 
-  resources :carriers
+  # resources :carriers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
