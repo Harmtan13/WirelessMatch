866c2bd95a5e089a434e567dab79ad249983a238
Tanner Harman <harmtan2@gmail.com>
2018-09-20 15:41:05 -0600

    Added Admin panel so I can look at different Users results-Temporary while in testing.

--- app/controllers/plan_recommendations_controller.rb
+++ app/controllers/plan_recommendations_controller.rb
@@ -20,4 +20,23 @@ class PlanRecommendationsController < ApplicationController
     @s = PlanGeneratorSprint.new(@sprint, @sprint_carrier_plans, @quiz)
   end
 
+  def admin
+    @quiz = UserPlan.last
+
+    @tmo = Carrier.find_by_name('T-Mobile')
+    @verizon = Carrier.find_by_name('Verizon')
+    @att = Carrier.find_by_name('AT&T')
+    @sprint = Carrier.find_by_name('Sprint')
+
+    @tmo_carrier_plans = @tmo.carrier_plans
+    @verizon_carrier_plans = @verizon.carrier_plans
+    @att_carrier_plans = @att.carrier_plans
+    @sprint_carrier_plans = @sprint.carrier_plans
+
+    @t = PlanGeneratorTmo.new(@tmo, @tmo_carrier_plans, @quiz)
+    @v = PlanGeneratorVerizon.new(@verizon, @verizon_carrier_plans, @quiz)
+    @a = PlanGeneratorAtt.new(@att, @att_carrier_plans, @quiz)
+    @s = PlanGeneratorSprint.new(@sprint, @sprint_carrier_plans, @quiz)
+  end
+
 end
