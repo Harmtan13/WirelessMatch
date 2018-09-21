b340620b5cfb92e9126c39e97d26a18cbba9ff07
Tanner Harman <harmtan2@gmail.com>
2018-09-20 18:57:59 -0600

    Added proper validation for user_line hd_video boolean value

--- app/models/user_line.rb
+++ app/models/user_line.rb
@@ -1,5 +1,7 @@
 class UserLine < ApplicationRecord 
-  validates_presence_of :data_amount, :hotspot, :hd_video
+  validates_presence_of :data_amount, :hotspot
+
+  validates_inclusion_of :hd_video, in: [true, false]
 
   belongs_to :user_plan, optional: true
 
